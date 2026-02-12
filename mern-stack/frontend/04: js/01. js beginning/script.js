6434fc2f203f07f79541a3363a4c94bfe68b4953
ImSurajx <surajxcode@gmail.com>
2026-02-13 02:44:13 +0530

    mern-stack: js beginning - status: just an overview of next class
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

---
+++ mern-stack/frontend/04: js/01. js beginning/script.js
@@ -0,0 +1,29 @@
+// introduction to js:
+// js is a scripting language, 
+// computer only understand binary,
+// you lanaguage -> interpreter/compiler -> 0101010101.
+
+// why it is important....
+// because we use js to create each type of interation through webpage..
+
+// what can it do for you? ->> engagement, interaction, animation, validation
+
+var a = 12;
+let b = 12;
+const c = 12;
+a = 32;
+b = 32;
+
+console.log("Hello");
+console.warn("don't do");
+console.error("404 not found");
+
+let str = "Hello Suraj";
+console.log(str.slice(0, 11));
+console.log(str.split('a'));
+console.log(str.replace('u', 'a'));
+console.log(str.includes('a'));
+
+
+
+
