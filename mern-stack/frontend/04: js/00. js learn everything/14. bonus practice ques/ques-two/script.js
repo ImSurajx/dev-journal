b0074908aab8473091cd172baf14cfe69f337c99
ImSurajx <surajxcode@gmail.com>
2026-02-26 23:26:56 +0530

    mern-stack: bonus practice question building: a card project which update accoriding to user action
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

---
+++ mern-stack/frontend/04: js/00. js learn everything/14. bonus practice ques/ques-two/script.js
@@ -0,0 +1,179 @@
+// create new card, save data into local storage
+// show cards from local storage
+// handle button
+// handle filtter
+
+/*
+- set listner on upButton
+    -> use stack & check it's latest elements
+    -> a, b, c
+    - extract lastChild..
+    - check is there any lastChild then insert it before firstChild
+    - update
+- set listner on downButton
+    - extract firstChild
+    - check is any firstChild then insert it after lastChild
+    - update
+
+- updatin: create a function updateStack(){}
+    - select all stack & card
+    - loop on that form 0 to 3
+        - card.style.zIndex = 3 - index
+        - card.style.transform = `translateY(${index * 10} scale(${1-index*0.02}))`;
+        - card.style.opacity = ${index * 10} scale(${1-index*0.02};
+*/
+
+// SELECT ELEMENTS
+let addNote = document.querySelector("#add-note");
+let formContainer = document.querySelector(".form-container");
+let closeForm = document.querySelector(".closeForm");
+const form = document.querySelector("#noteForm")
+const { imageUrl, fullName, homeTown, purpose, category } = form.elements;
+let textField = [imageUrl, fullName, homeTown, purpose, category];
+const stack = document.querySelector('.stack');
+const upBtn = document.querySelector("#upBtn");
+const downBtn = document.querySelector("#upBtn");
+
+
+
+
+addNote.addEventListener("click", function () {
+    formContainer.style.display = "initial";
+    closeForm.addEventListener("click", function () {
+        formContainer.style.display = "none";
+    })
+});
+
+form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    for (let field of textField) {
+        if (field.value.trim() == "") {
+            alert(`Enter ${field.name}`);
+            return;
+        }
+    }
+    let selectCategory = form.querySelector('input[name="category"]:checked');
+    if (!selectCategory) {
+        alert("Select a category");
+        return;
+    }
+    saveToLocal({
+        "imageUrl": imageUrl.value,
+        "fullName": fullName.value,
+        "homeTown": homeTown.value,
+        "purpose": purpose.value,
+        "category": selectCategory.value,
+    });
+    showCards();
+    formContainer.style.display = "none";
+    form.reset();
+});
+
+function saveToLocal(obj) {
+    let tasks = localStorage.getItem("tasks");
+    // if (!tasks) {
+    //     let objArr = [];
+    //     objArr.push(obj);
+    //     localStorage.setItem("tasks", JSON.stringify(objArr));
+    // }
+    // else {
+    //     let objArr = JSON.parse(tasks);
+    //     objArr.push(obj);
+    //     localStorage.setItem("tasks", JSON.stringify(objArr));
+    // }
+
+    // cleaner version
+    let objArr = [];
+
+    if (tasks) {
+        try {
+            objArr = JSON.parse(tasks);
+        } catch (error) {
+            objArr = [];
+        }
+    }
+
+    objArr.push(obj);
+    localStorage.setItem("tasks", JSON.stringify(objArr));
+
+}
+
+function showCards() {
+    let allTasks = JSON.parse(localStorage.getItem("tasks"));
+    allTasks.forEach(function (task) {
+        // create card container
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        // avatar image
+        const avatar = document.createElement('img');
+        avatar.src = task.imageUrl;
+        avatar.alt = "profile";
+        avatar.classList.add("avatar");
+        card.appendChild(avatar);
+
+        // name
+        const name = document.createElement("h2");
+        name.textContent = task.fullName;
+        card.appendChild(name);
+
+        // info : home town
+        const homeTownInfo = document.createElement("div");
+        homeTownInfo.classList.add("info");
+
+        const homeTownLabel = document.createElement("span");
+        homeTownLabel.textContent = "Home town";
+
+        const homeTownValue = document.createElement("span");
+        homeTownValue.textContent = task.homeTown;
+
+        homeTownInfo.appendChild(homeTownLabel);
+        homeTownInfo.appendChild(homeTownValue);
+        card.appendChild(homeTownInfo);
+
+        // info: purpose
+        const bookingsInfo = document.createElement("div");
+        bookingsInfo.classList.add("info");
+
+        const bookingsLabel = document.createElement("span");
+        bookingsLabel.textContent = "Purpose";
+
+        const bookingsValue = document.createElement("span");
+        bookingsValue.textContent = task.purpose;
+
+        bookingsInfo.appendChild(bookingsLabel);
+        bookingsInfo.appendChild(bookingsValue);
+
+        card.appendChild(bookingsInfo);
+
+        // Buttons container
+        const buttonsDiv = document.createElement("div");
+        buttonsDiv.classList.add("buttons");
+
+        // Call button
+        const callBtn = document.createElement("div");
+        callBtn.classList.add("call");
+        callBtn.innerHTML = '<i class="ri-phone-line"></i> Call';
+        
+        // Message button
+        const msgBtn = document.createElement("div");
+        callBtn.innerHTML = '<i class="ri-message-line"></i> Message';
+        msgBtn.classList.add("message");
+
+        // append buttons
+        buttonsDiv.appendChild(callBtn);
+        buttonsDiv.appendChild(msgBtn);
+
+        // append buttonsDiv to card
+        card.appendChild(buttonsDiv);
+
+        // add card to stack
+        stack.appendChild(card);
+    });
+
+}
+
+upBtn.addEventListener("click", function(){
+    
+})
+
