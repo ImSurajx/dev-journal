a33c8e118b3cd0cd4f4abeb7a3bab1a8c8347449
ImSurajx <surajxcode@gmail.com>
2026-03-18 21:14:45 +0530

    mern-stack: js design patterns
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- mern-stack/frontend/04: js/00. js learn everything/22. design patterns/script.js
+++ mern-stack/frontend/04: js/00. js learn everything/22. design patterns/script.js
@@ -1,73 +1,73 @@
-// // design patterns
+// design patterns
 
-// // module pattern: a design pattern in which our code is writen inside self executed function (IIFE) so our varibles can be privte.
-// // we only return those things which we want to use outside.
-// // than main benift of this pattern is data hiding(encapsulation) and clean strucutre, so our code can be secure, reusable aur manageble
+// module pattern: a design pattern in which our code is writen inside self executed function (IIFE) so our varibles can be privte.
+// we only return those things which we want to use outside.
+// than main benift of this pattern is data hiding(encapsulation) and clean strucutre, so our code can be secure, reusable aur manageble
 
-// // create iffe
-// let  Bank = (function(){
-//     let bankbalance = 12000; // private varible
-//     function checkBalance(){
-//         console.log(bankbalance);
-//     }
-//     function setBalance(val){
-//         bankbalance = val;
-//     }
-//     function withdraw(val){
-//         if(val<=bankbalance) bankbalance -= val;
-//         console.log(bankbalance);
+// create iffe
+let  Bank = (function(){
+    let bankbalance = 12000; // private varible
+    function checkBalance(){
+        console.log(bankbalance);
+    }
+    function setBalance(val){
+        bankbalance = val;
+    }
+    function withdraw(val){
+        if(val<=bankbalance) bankbalance -= val;
+        console.log(bankbalance);
 
-//     }
-//     // we return object, so we can access function of IFFE.
-//     return {
-//         checkBalance,
-//         setBalance,
-//         withdraw,
-//     }
-// })();
-// console.log(Bank.checkBalance());
-// console.log(Bank.withdraw(1000));
-// console.log(Bank.checkBalance());
+    }
+    // we return object, so we can access function of IFFE.
+    return {
+        checkBalance,
+        setBalance,
+        withdraw,
+    }
+})();
+console.log(Bank.checkBalance());
+console.log(Bank.withdraw(1000));
+console.log(Bank.checkBalance());
 
-// // revealing module pattern
-// let  BankReveal = (function(){
-//     let bankbalance = 12000; // private varible
-//     function checkBalance(){
-//         console.log(bankbalance);
-//     }
-//     function setBalance(val){
-//         bankbalance = val;
-//     }
-//     function withdraw(val){
-//         if(val<=bankbalance) bankbalance -= val;
-//         console.log(bankbalance);
+// revealing module pattern
+let  BankReveal = (function(){
+    let bankbalance = 12000; // private varible
+    function checkBalance(){
+        console.log(bankbalance);
+    }
+    function setBalance(val){
+        bankbalance = val;
+    }
+    function withdraw(val){
+        if(val<=bankbalance) bankbalance -= val;
+        console.log(bankbalance);
 
-//     }
-//     // we return object, so we can access function of IFFE.
-//     // revealing: we can send custom names
-//     return {
-//         check: checkBalance,
-//         set: setBalance,
-//         draw: withdraw,
-//     }
-// })();
-// console.log(BankReveal.check());
-// console.log(BankReveal.draw(1000));
-// console.log(BankReveal.check());
-
-
+    }
+    // we return object, so we can access function of IFFE.
+    // revealing: we can send custom names
+    return {
+        check: checkBalance,
+        set: setBalance,
+        draw: withdraw,
+    }
+})();
+console.log(BankReveal.check());
+console.log(BankReveal.draw(1000));
+console.log(BankReveal.check());
 
 
-// factory function pattern
-// a function which creates a object (function = object creation machine)
 
-// a desing pattern in which we write a simple function which create new objects & return it without using class & new keyword.
 
-// this pattern mean control object creation through our function.
+factory function pattern
+a function which creates a object (function = object creation machine)
 
-// each time when we call a factory function, it return a new object each time in which that object has it's own methods also your private data.
+a desing pattern in which we write a simple function which create new objects & return it without using class & new keyword.
 
-// this pattern is usefull when you are needed similar type of objects like users, products, tasks, etc.
+this pattern mean control object creation through our function.
+
+each time when we call a factory function, it return a new object each time in which that object has it's own methods also your private data.
+
+this pattern is usefull when you are needed similar type of objects like users, products, tasks, etc.
 
 function createProduct(name, price) {
     let stock = 10;
@@ -95,6 +95,41 @@ function createProduct(name, price) {
 
 let iPhone = createProduct("iPhone", 70000);
 console.log(iPhone);
+
+// Observer Pattern 
+class YoutubeChannel {
+    constructor() {
+        this.subscriber = [];
+    }
+    subscribe(user) {
+        this.subscriber.push(user);
+        user.update(`you have subscribed the channel.`);
+    }
+    ussubscribe(user) {
+        this.subscriber = this.subscriber.filter((sub) => sub !== user);
+        user.update(`you have unsubscribed the channel.`);
+
+    }
+    notify(message) {
+        this.subscriber.forEach(sub => sub.update(message))
+    }
+}
+
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+    update(data) {
+        console.log(`${this.name}, ${data}`);
+    }
+}
+
+let evolution = new YoutubeChannel();
+let user1 = new User("Suraj");
+let user2 = new User("Amit");
+evolution.subscribe(user1);
+evolution.subscribe(user2);
+evolution.notify("new videos live on the channel.")
 
 
 
