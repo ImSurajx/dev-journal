43138fbe63d21d54a51292aff6842ba265e22e44
ImSurajx <surajxcode@gmail.com>
2026-04-09 21:03:36 +0530

    dsa-x-cpp: revise some problems
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- dsa-x-cpp/09: Searching/02. Binary Search Probems/leetCode81.cpp
+++ dsa-x-cpp/09: Searching/02. Binary Search Probems/leetCode81.cpp
@@ -1,4 +1,4 @@
-// ques: search in rotated sorted array
+// ques: search in rotated sorted array, array contains duplicate elements
 #include <iostream>
 using namespace std;
 int search(vector<int> &nums, int target)
@@ -10,9 +10,15 @@ int search(vector<int> &nums, int target)
         int mid = low + (high - low) / 2;
         if (nums[mid] == target)
             return mid;
+        // if all element are same that mean we lost our power of binary search so to gain that power again on all point if element are same we will move our low or high pointer
+        else if (nums[low] == nums[mid] && nums[mid] == nums[high])
+        {
+            low++;
+            high--;
+        }
+        // right part of array is sorted
         else if (nums[mid] <= nums[high])
         {
-            // right part of array is sorted
             // check is target value belong to the right part of array or not?
             if (target >= nums[mid] && target <= nums[high])
             {
@@ -23,9 +29,9 @@ int search(vector<int> &nums, int target)
                 high = mid - 1;
             }
         }
+        // this mean left part is sorted in the array
         else
         {
-            // this mean left part is sorted in the array
             // check is target value belong to the left part of array or not?
             if (target >= nums[low] && target < nums[mid])
             {
@@ -42,8 +48,8 @@ int search(vector<int> &nums, int target)
 
 int main()
 {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-    int getElementIdx = search(nums, 0);
+    vector<int> nums = {4, 5, 6, 7, 8, 0, 0, 0, 1, 2};
+    int getElementIdx = search(nums, 6);
     cout << "the element found at idx: " << getElementIdx << endl;
     return 0;
 }
