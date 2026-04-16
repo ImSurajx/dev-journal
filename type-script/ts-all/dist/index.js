90f7eb67b05a05469f019365652b8aad3f2eabd5
ImSurajx <surajxcode@gmail.com>
2026-04-16 11:56:40 +0530

    type-script: annotation and inference
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

---
+++ type-script/ts-all/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let drink = "chai";
+let cups = Math.random() > 0.5 ? 10 : 5;
+let channelName = "Suraj Kumar";
+let chaiFlaviour = "masala chai";
+chaiFlaviour = "Ginger Tea";
+let chaiOrder = true;
+// anotation: which we have to define.
+// infrance: which we don't have to define.
+//# sourceMappingURL=index.js.map
