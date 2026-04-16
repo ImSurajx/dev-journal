8d9f0e44b3606ba17bfe95d9e56fbf5f543d4756
ImSurajx <surajxcode@gmail.com>
2026-04-16 10:54:39 +0530

    type-script: introduction
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

---
+++ type-script/basic.ts
@@ -0,0 +1,14 @@
+function greet(name: string): string {
+    return `Hello, ${name}`;
+}
+console.log(greet("Suraj Kumar"));
+// console.log(greet(42));
+
+
+// Freedom:
+// loose docs: from code to doc it's hard to convert.
+// developer tooling: unusal behaviour issue...
+
+// TypeScript:
+// TS: add-on over javascript, ts need a process then we get a js..
+// TypeChecker: check data types
