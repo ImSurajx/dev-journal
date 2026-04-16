0fecdafb2910304c86026b0fbdecd08eb264c1d9
ImSurajx <surajxcode@gmail.com>
2026-04-16 12:06:59 +0530

    type-script: union or any
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- type-script/ts-all/dist/index.js
+++ type-script/ts-all/dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let drink = "chai";
-let cups = Math.random() > 0.5 ? 10 : 5;
-let channelName = "Suraj Kumar";
-let chaiFlaviour = "masala chai";
-chaiFlaviour = "Ginger Tea";
-let chaiOrder = true;
-// anotation: which we have to define.
-// infrance: which we don't have to define.
+// Union Types
+let subs = '1M';
+let apiRequestStatus = 'pending';
+apiRequestStatus = "success";
+let airlineSeat = 'aisle';
+airlineSeat = 'aisle';
+// Any
+const orders = ['12', '20', '32', '45'];
+let currentOrder;
+for (let order of orders) {
+    if (order === '42') {
+        currentOrder = order;
+        break;
+    }
+}
+console.log(currentOrder);
 //# sourceMappingURL=index.js.map
