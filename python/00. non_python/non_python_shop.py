f6e73365325abf7c6b6bc9f4aa5a7d3c37f4d633
ImSurajx <surajxcode@gmail.com>
2026-04-22 20:01:53 +0530

    python: experience the python
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

---
+++ python/00. non_python/non_python_shop.py
@@ -0,0 +1,14 @@
+class Chai:
+    def __init__(self, sweetness, milk_level):
+        self.sweetness = sweetness
+        self.mil_level = self.mil_level
+
+    def sip(self):
+        prit("Shipping chai")
+
+    def add_sugar(self, amount):
+        print("addes the sugar")
+
+
+my_chai = Chai(sweetness=3, milk_level=2)
+my_cahi.add_sugar(3)
