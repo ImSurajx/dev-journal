ccd3969e59fab895bb6f9c87e6df797e88d16f60
ImSurajx <surajxcode@gmail.com>
2026-04-23 11:10:57 +0530

    typescript: union or any
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -5,4 +5,28 @@ function greet(person) {
 }
 const username = "ImSurajx";
 console.log(greet(username));
+// types in ts:
+// inference: auto detect type
+let drink = "chai";
+let cups = Math.random() > 0.5 ? 10 : '5';
+let channelName = "SurajKumar";
+// type anoataion: define type manually,
+let chaiFlavour = "Masala Chai";
+chaiFlavour = "Ginger Tea";
+let chaiOrder;
+// Union and Any
+let subs = '1M';
+// on production: define value & use it..
+let apiRequestStatus = 'pending';
+apiRequestStatus = 'success';
+let airLineSeat = 'middle'; // you can only give any three value out of these..
+airLineSeat = 'window';
+const orders = ['12', '20', '28', '42'];
+let currentOrder; // we can also create our own data type...
+for (let order of orders) {
+    if (order == '28') {
+        currentOrder = order; // any: i don't care about data types
+    }
+}
+console.log(currentOrder); // string or undefined.
 //# sourceMappingURL=index.js.map
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -11,7 +11,26 @@ let drink = "chai"
 let cups = Math.random() > 0.5 ? 10 : '5';
 let channelName = "SurajKumar";
 
-// type annoataion: define type manually,
+// type anoataion: define type manually,
 let chaiFlavour: string = "Masala Chai";
 chaiFlavour = "Ginger Tea"
 let chaiOrder: boolean;
+
+// Union and Any
+let subs: number | string = '1M';
+
+// on production: define value & use it..
+let apiRequestStatus: 'pending' | 'success' | 'error' = 'pending';
+apiRequestStatus = 'success'
+
+let airLineSeat: 'aisle' | 'window' | 'middle' = 'middle'; // you can only give any three value out of these..
+airLineSeat = 'window'
+
+const orders = ['12', '20', '28', '42'];
+let currentOrder: string | undefined; // we can also create our own data type...
+for (let order of orders) {
+    if (order == '28') {
+        currentOrder = order; // any: i don't care about data types
+    }
+}
+console.log(currentOrder); // string or undefined.
