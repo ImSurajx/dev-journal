112f61d0fa93aa6e4d931e4c116514534b646c36
ImSurajx <surajxcode@gmail.com>
2026-04-23 11:40:23 +0530

    typescript: type narrowing or custom types
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -29,4 +29,75 @@ for (let order of orders) {
     }
 }
 console.log(currentOrder); // string or undefined.
+// type narrowing: we close to know the real suggestions
+function getChai(kind) {
+    if (typeof kind == 'string') {
+        return `making ${kind} chai...`;
+    }
+    return `chai order: ${kind}`;
+}
+// truthyness
+function saveChai(msg) {
+    if (msg) {
+        return `serving ${msg} `;
+    }
+    return `serving default masala chai`;
+}
+// exostive checks
+function orderChai(size) {
+    if (size === 'small')
+        return `small cutting chai`;
+    else if (size === 'medium' || size === 'large')
+        return `make extra chai`;
+    else
+        return `chai order #${size}`;
+}
+class KulhadChai {
+    serve() {
+        return `serving kulhad chai`;
+    }
+}
+class Cutting {
+    serve() {
+        return `serving cutting chai`;
+    }
+}
+function serve(chai) {
+    if (chai instanceof KulhadChai) {
+        return chai.serve();
+    }
+    return chai.serve();
+}
+// if anything go through this function that mean we can also use type or sugar keys on that value too..
+function isChaiOrder(obj) {
+    return (typeof obj == 'object' &&
+        obj != null &&
+        typeof obj.type == "string" &&
+        typeof obj.sugar === "number");
+}
+function serveOrder(item) {
+    if (isChaiOrder(item)) {
+        return `Serving ${item.type} chai with ${item.sugar} sugar`;
+    }
+    return `serving custom chai ${item}`;
+}
+// pass that type into & function according to your needs
+function makeChai(order) {
+    switch (order.type) {
+        case 'masala':
+            return 'masala chai';
+        case 'elaichi':
+            return 'elaichi chai';
+        case 'ginger':
+            return 'ginger chai';
+    }
+}
+function brew(order) {
+    if ('spicelevel' in order) {
+        return `masala chai`;
+    }
+}
+// function isStringArray(arr: unknown): arr is string[]{
+//     // return arr[];
+// }
 //# sourceMappingURL=index.js.map
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -34,3 +34,94 @@ for (let order of orders) {
     }
 }
 console.log(currentOrder); // string or undefined.
+
+// type narrowing: we close to know the real suggestions
+function getChai(kind: string | number) {
+    if (typeof kind == 'string') {
+        return `making ${kind} chai...`
+    }
+    return `chai order: ${kind}`;
+}
+
+// truthyness
+function saveChai(msg?: string) {
+    if (msg) {
+        return `serving ${msg} `;
+    }
+    return `serving default masala chai`;
+}
+
+// exostive checks
+function orderChai(size: 'medium' | 'small' | 'large' | number) {
+    if (size === 'small') return `small cutting chai`;
+    else if (size === 'medium' || size === 'large') return `make extra chai`;
+    else return `chai order #${size}`;
+}
+
+class KulhadChai {
+    serve() {
+        return `serving kulhad chai`
+    }
+}
+class Cutting {
+    serve() {
+        return `serving cutting chai`
+    }
+}
+
+function serve(chai: KulhadChai | Cutting) {
+    if (chai instanceof KulhadChai) {
+        return chai.serve();
+    }
+    return chai.serve();
+}
+
+// create your own type:
+type ChaiOrder = {
+    type: string,
+    sugar: number,
+}
+// if anything go through this function that mean we can also use type or sugar keys on that value too..
+function isChaiOrder(obj: any): obj is ChaiOrder {
+    return (
+        typeof obj == 'object' &&
+        obj != null &&
+        typeof obj.type == "string" &&
+        typeof obj.sugar === "number"
+    )
+}
+
+function serveOrder(item: ChaiOrder | string) {
+    if (isChaiOrder(item)) {
+        return `Serving ${item.type} chai with ${item.sugar} sugar`;
+    }
+    return `serving custom chai ${item}`;
+}
+
+// create custom type
+type MasalaChai = { type: 'masala'; spicelevel: number }
+type GingerChai = { type: 'ginger'; amount: number }
+type ElaichiChai = { type: 'elaichi'; aroma: number }
+// use that custom type as a variable value in another type
+type chai = MasalaChai | GingerChai | ElaichiChai;
+// pass that type into & function according to your needs
+function makeChai(order: chai) {
+    switch (order.type) {
+        case 'masala':
+            return 'masala chai'
+        case 'elaichi':
+            return 'elaichi chai'
+        case 'ginger':
+            return 'ginger chai'
+    }
+}
+
+function brew(order: MasalaChai | GingerChai) {
+    if ('spicelevel' in order) {
+        return `masala chai`;
+    }
+}
+
+function isStringArray(arr: unknown): arr is string[]{
+    // return arr[];
+}
