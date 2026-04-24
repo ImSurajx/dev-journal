11e91814b5b4895a672340812144516638c76437
ImSurajx <surajxcode@gmail.com>
2026-04-24 17:34:12 +0530

    react-js: hello world in react js
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

---
+++ react-js/episode1: hello world in react/App.js
@@ -0,0 +1,19 @@
+// create a ReactElement
+const container = React.createElement("div", { id: "container" },
+   [
+    React.createElement('div', {id: "container1"}, 
+        [
+        React.createElement('h1', { id: "h1" }, "Hello, I'm Heading One"),
+        React.createElement('h2', { id: "h2" }, "Hello, I'm Heading Two")
+        ]
+    ),
+    React.createElement('div', {id: "container2"}, 
+        [
+        React.createElement('h1', { id: "h1" }, "Hello, I'm Heading One"),
+        React.createElement('h2', { id: "h2" }, "Hello, I'm Heading Two")
+        ]
+    )
+   ]
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container)
